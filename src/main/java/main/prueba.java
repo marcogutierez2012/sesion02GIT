b4735a4148ac03b1022b03f6981b2fa3fd4e0e83
marco <marcogutierrez@gmail.com>
2023-09-11 21:54:01 -0500

    Listar usuarios mediante llamada procedimiento almacenado

--- src/main/java/main/prueba.java
+++ src/main/java/main/prueba.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import model.TbUsuario;
 
@@ -15,10 +16,15 @@ public class prueba {
 		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("JPA_clase01x");
 		EntityManager em = fabrica.createEntityManager();
 		
-		List<TbUsuario> lstUsuarios = em.createQuery("Select t From TbUsuario t", TbUsuario.class).getResultList();
+		Query query = em.createNativeQuery("{call findAll_usuarios()}", TbUsuario.class);
+		
+		List<TbUsuario> lstUsuarios = query.getResultList();
+		
 		System.out.println("Número de usuarios : " + lstUsuarios.size());
-
-		//hola
+		System.out.println("==========================================");
+		//List<TbUsuario> lstUsuarios = em.createQuery("Select t From TbUsuario t", TbUsuario.class).getResultList();
+		//System.out.println("Número de usuarios : " + lstUsuarios.size());
+		
 		
 		
 	}
